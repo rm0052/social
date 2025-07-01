bd6eb69237355bf0a5de2b1e4993fa345c6144a5
Rohan Mani <64933913+rm0052@users.noreply.github.com>
2025-07-01 15:55:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ reddit = praw.Reddit(
     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
     username=os.getenv("REDDIT_USERNAME"),
     password=os.getenv("REDDIT_PASSWORD"),
+    user_agent="silverbot by u/Recent_Body981"
 )
 
 # --- App Setup ---
